2cede75a0524bad5a3f80410bbed840dba8b24ad
dkjha108 <brajeshjha108@gmail.com>
2021-11-17 16:17:03 +0530

    logo svg issue

--- components/general/Logo.jsx
+++ components/general/Logo.jsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import logo from "../../public/behairful-logo-main.svg";
+import logomobile from "../../public/behairful-logo-main.svg";
 
 const Logo = () => {
     return (
-        <header style={{textAlign: "center", padding: "2.5rem"}}>
-            <Image src={logo} width={'259px'} height={'146px'} alt={'behairful-logo'} />
+        <header class="logologin" style={{textAlign: "center"}}>
+            <Image src={logo} width={'259px'} height={'146px'} alt={'behairful-logo'} class="logologindesign" />
+            <div class="signupheader"> 
+                <span>Partner hub</span><Image src={logomobile} width={'88px'} height={'88px'} alt={'behairful-logo'} class="logolmobile" />
+            </div>
         </header>
     );
 }
