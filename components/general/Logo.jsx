778774319cdbf32d7fcb9365f12ada40ac17a349
dkjha108 <brajeshjha108@gmail.com>
2021-11-17 16:38:09 +0530

    test logo

--- components/general/Logo.jsx
+++ components/general/Logo.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import logo from "../../public/behairful-logo-main.svg";
+import logomain from "../../public/behairful-logo-main.svg";
 import logomobile from "../../public/behairful-partners-logo-ai-mobile.svg";
 
 const Logo = () => {
     return (
         <header style={{textAlign: "center", padding: "2.5rem"}}>
-            <Image src={logo} width={'259px'} height={'146px'} alt={'behairful-logo'} />
+            <Image src={logomain} width={'259px'} height={'146px'} alt={'behairful-logo'} />
             <div class="signupheader"> 
                 <span>Partner hub</span><Image src={logomobile} width={'88px'} height={'88px'} alt={'behairful-logo'} class="logolmobile" />
             </div>
